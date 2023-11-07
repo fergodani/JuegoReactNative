22f8dfbc177e71e2522e63c11715ced138f2a17d
jserras98 <xuansp98@gmail.com>
2023-11-07 17:48:42 +0100

    Añadido lagarto-spock

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import HeaderImage from './images/lagarto_spock.png';
 
 class PiedraPapelTijeraApp extends Component {
   constructor() {
@@ -10,16 +11,18 @@ class PiedraPapelTijeraApp extends Component {
   }
 
   jugar(opcionUsuario) {
-    const opciones = ['piedra', 'papel', 'tijera'];
+    const opciones = ['piedra', 'papel', 'tijera','lagarto','spock'];
     const opcionAleatoria = opciones[Math.floor(Math.random() * 3)];
     
     if (opcionUsuario === opcionAleatoria) {
       this.setState({ resultado: 'Empate' });
     } else if (
-      (opcionUsuario === 'piedra' && opcionAleatoria === 'tijera') ||
-      (opcionUsuario === 'papel' && opcionAleatoria === 'piedra') ||
-      (opcionUsuario === 'tijera' && opcionAleatoria === 'papel')
-    ) {
+      ((opcionUsuario === 'piedra' && (opcionAleatoria === 'tijera' || opcionAleatoria === 'lagarto')) ||
+      (opcionUsuario === 'papel' && (opcionAleatoria === 'piedra' || opcionAleatoria === 'spock')) ||
+      (opcionUsuario === 'tijera' && (opcionAleatoria === 'papel' || opcionAleatoria === 'lagarto')) ||
+      (opcionUsuario === 'lagarto' && (opcionAleatoria === 'papel' || opcionAleatoria === 'spock')) ||
+      (opcionUsuario ==='spock' && (opcionAleatoria === 'piedra' || opcionAleatoria === 'tijera')))
+     ) {
       this.setState({ resultado: 'Ganaste' });
     } else {
       this.setState({ resultado: 'Perdiste' });
@@ -29,10 +32,15 @@ class PiedraPapelTijeraApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Elige: piedra, papel o tijera</Text>
+        <View style={styles.header}>
+          <Image source={HeaderImage} style={styles.headerImage} />
+        </View>
+        <Text>Piedra, papel, tijera, lagarto, spock</Text>
         <Button title="Piedra" onPress={() => this.jugar('piedra')} />
         <Button title="Papel" onPress={() => this.jugar('papel')} />
         <Button title="Tijera" onPress={() => this.jugar('tijera')} />
+        <Button title="Lagarto" onPress={() => this.jugar('lagarto')} />
+        <Button title="Spock" onPress={() => this.jugar('spock')} />
         <Text>Resultado: {this.state.resultado}</Text>
       </View>
     );
@@ -44,6 +52,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  headerImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'cover',
   },
 });
 
