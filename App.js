e5a78a708b049b0a5df92ffe0f1b125c09debb01
Daniel Ferreira Gómez <ferreiragomezdaniel@gmail.com>
2023-11-07 17:10:24 +0100

    Juego inicializado

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class PiedraPapelTijeraApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      resultado: '',
+    };
+  }
+
+  jugar(opcionUsuario) {
+    const opciones = ['piedra', 'papel', 'tijera'];
+    const opcionAleatoria = opciones[Math.floor(Math.random() * 3)];
+    
+    if (opcionUsuario === opcionAleatoria) {
+      this.setState({ resultado: 'Empate' });
+    } else if (
+      (opcionUsuario === 'piedra' && opcionAleatoria === 'tijera') ||
+      (opcionUsuario === 'papel' && opcionAleatoria === 'piedra') ||
+      (opcionUsuario === 'tijera' && opcionAleatoria === 'papel')
+    ) {
+      this.setState({ resultado: 'Ganaste' });
+    } else {
+      this.setState({ resultado: 'Perdiste' });
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Elige: piedra, papel o tijera</Text>
+        <Button title="Piedra" onPress={() => this.jugar('piedra')} />
+        <Button title="Papel" onPress={() => this.jugar('papel')} />
+        <Button title="Tijera" onPress={() => this.jugar('tijera')} />
+        <Text>Resultado: {this.state.resultado}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default PiedraPapelTijeraApp;
